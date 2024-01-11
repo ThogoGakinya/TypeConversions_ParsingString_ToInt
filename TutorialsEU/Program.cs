278b55f9eaae7f60bf1aca54d975199e5f3190d3
ThogoGakinya <thogosamson@gmail.com>
2024-01-11 19:59:58 +0300

    Add project files.

---
+++ TutorialsEU/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TutorialsEU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float myFloat = 32.57f;
+            int myInteger;
+
+            //explicit convertion
+            myInteger = (int)myFloat;
+
+            //implicit convertion
+            double myDouble = myFloat;
+            long myLong = myInteger;
+
+            //type convertion
+            string myString = myFloat.ToString();
+
+            //parsing strings into integers
+
+            string myFirst = "15";
+            int myFirstInt = Int32.Parse(myFirst);
+            string mySecond = "17";
+            int mySecondInt = Int32.Parse(mySecond);
+           
+            int results = myFirstInt + mySecondInt;
+
+            Console.WriteLine(results);
+            Console.Read();
+        }
+    }
+}
